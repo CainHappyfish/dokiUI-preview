50e38fc5fdd81c8b57d267cd28e9a69d638cd3bc
CainHappyfish <119086094+CainHappyfish@users.noreply.github.com>
2024-10-24 15:22:57 +0800

    fix: build

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// ts-ignore
 import DokiUI from "doki-ui/dist/doki-ui.js"
 import "doki-ui/dist/style.css"
 import "doki-ui/src/styles/index.scss"
